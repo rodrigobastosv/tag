e703511fcc97f10d44d7f794ca762f15f4147f3e
rodrigobastosv <rodrigobastosv@gmail.com>
2021-03-02 13:28:19 -0300

    migrating to null safe

--- lib/src/tag.dart
+++ lib/src/tag.dart
@@ -4,9 +4,9 @@ import 'clippers/triangle_clipper.dart';
 
 class Tag extends StatelessWidget {
   Tag({
-    Key key,
-    @required this.child,
-    @required this.text,
+    Key? key,
+    required this.child,
+    required this.text,
     this.textStyle,
     this.labelDecoration,
     this.color,
@@ -19,55 +19,68 @@ class Tag extends StatelessWidget {
     this.isVisible = true,
   }) : super(key: key);
 
+  /// Widget that will receive the tag
   final Widget child;
+
+  /// Text to go on the tag if any
   final String text;
-  final TextStyle textStyle;
-  final BoxDecoration labelDecoration;
-  final Color color;
+
+  /// Style of the text
+  final TextStyle? textStyle;
+  final BoxDecoration? labelDecoration;
+  final Color? color;
+
+  /// Height of the tag
   final double tagHeight;
+
+  /// Width of the tag
   final double tagWidth;
+
   final double cornerHeight;
   final double cornerWidth;
   final double spacing;
   final double rightPadding;
+
+  /// Wheter the tag is visible or not
   final bool isVisible;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
-      overflow: Overflow.visible,
+      clipBehavior: Clip.none,
       children: [
         child,
-        if (isVisible) Positioned(
-          right: rightPadding,
-          bottom: -spacing - cornerHeight - tagHeight,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ClipPath(
-                clipper: TriangleClipper(),
-                child: Container(
-                  color: color,
-                  height: cornerHeight,
-                  width: cornerWidth,
-                ),
-              ),
-              Container(
-                decoration: labelDecoration?.copyWith(
-                  color: color,
-                ),
-                height: tagHeight,
-                width: tagWidth,
-                child: Center(
-                  child: Text(
-                    text,
-                    style: textStyle,
+        if (isVisible)
+          Positioned(
+            right: rightPadding,
+            bottom: -spacing - cornerHeight - tagHeight,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ClipPath(
+                  clipper: TriangleClipper(),
+                  child: Container(
+                    color: color,
+                    height: cornerHeight,
+                    width: cornerWidth,
                   ),
                 ),
-              ),
-            ],
+                Container(
+                  decoration: labelDecoration?.copyWith(
+                    color: color,
+                  ),
+                  height: tagHeight,
+                  width: tagWidth,
+                  child: Center(
+                    child: Text(
+                      text,
+                      style: textStyle,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
       ],
     );
   }
